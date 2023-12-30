f5523a3ae9bd0e8bd3da4a1377826b1eda17f047
Agerri <agerrikabeitia@abraprod.com>
2023-12-30 16:57:21 +0100

    Actualizar

---
+++ 03_ejercicio_crear_objeto/script.js
@@ -0,0 +1,14 @@
+let Coche = {
+  Color: "rojo",
+  Marca: "Ford",
+  Modelo: "Focus",
+  Motor: {
+    cilindros: 4,
+    capacidad: 2.2,
+  },
+};
+console.log(Coche.Color);
+console.log(Coche.Marca);
+console.log(Coche.Modelo);
+console.log(Coche.Motor.cilindros);
+console.log(Coche.Motor.capacidad);
---
+++ 07_ejercicio_añadir_atributos/script.js
@@ -0,0 +1,15 @@
+let persona = {
+  nombre: "Alberto",
+  edad: 58,
+};
+
+console.log(persona);
+console.log(persona.nombre);
+console.log(persona.edad);
+
+persona.altura = 1.84;
+
+console.log(persona);
+console.log(persona.nombre);
+console.log(persona.edad);
+console.log(persona.altura);
